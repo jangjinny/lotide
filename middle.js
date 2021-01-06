f325f38fb9e9200cb6eb5c5d04ddc70e37aa5eb9
Jinny Jang <jangjinny21@gmail.com>
2021-01-06 22:44:06 +0000

    rearragned lines

--- middle.js
+++ middle.js
@@ -23,16 +23,13 @@ const assertArrayEqual = function(array1, array2) {
 //return the middle elements of an array
 const middle = function(array) {
   let middleNum = [];
-  //array with one or two elements should return an empty array
-  if (array.length === 1 || array.length === 2) {
-    return middleNum;
-  }
-
   let middleIndex1 = Math.floor(array.length / 2); //returns index of middle number
   let middleIndex2 = Math.floor((array.length - 1) / 2);
 
   //array with odd number elements should return a single element
-  if (array.length % 2 !== 0) {
+  if (array.length === 1 || array.length === 2) {
+    return middleNum;
+  } else if (array.length % 2 !== 0) {
     middleNum.push(array[middleIndex1]);
     return middleNum;
   } else {
