7b274b7758bd9271616046f4dc3eb0fb4a8f82cd
Jinny Jang <jangjinny21@gmail.com>
2021-01-07 03:45:47 +0000

    function to count the letters in a sentence

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const countLetters = function(sentence) {
+  const stats = {};
+
+  for (const letter of sentence) {
+    if (stats[letter]) {
+      stats[letter] += 1;
+    } else {
+      stats[letter] = 1;
+    }
+  }
+  return stats;
+};
+
+console.log(countLetters("lighthouse in the house"));
