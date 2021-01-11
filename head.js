863b4379957e2198c89a0b49a26c85f489ad6041
Jinny Jang <jangjinny21@gmail.com>
2021-01-11 20:57:58 +0000

    added exports

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
+const assertEqual = require('./assertEqual');
+//results in extra tests printing 
+//comes from assertEqual.js file
 
 const head = function(array) {
   return array[0];
 };
 
-
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head; //note were passing a variable, not calling a function()
