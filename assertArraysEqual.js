8c8c9f8e0e96ccd1cd66c117db0863c4494cba7c
Jinny Jang <jangjinny21@gmail.com>
2021-01-11 22:35:28 +0000

    added require eqArrays and exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-  //compare lengths of array1 and array2
-  if (array1.length === array2.length) {
-    //compare contents of array1 and array2
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,3 +7,5 @@ const assertArrayEqual = function(array1, array2) {
     return `❌❌❌ Assertion Failed: ${array1} !== ${array2}`;
   }
 };
+
+module.exports = assertArrayEqual;
