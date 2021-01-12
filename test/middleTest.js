679632bc4a044c378e5c29b2d3a453712bd68fa2
Jinny Jang <jangjinny21@gmail.com>
2021-01-12 00:38:14 +0000

    added mocha functions to test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,27 @@
 const middle = require('../middle');
 const assertArrayEqual = require('../assertArrayEqual');
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual');
 
-console.log(assertArrayEqual(middle([]), []));
-console.log(assertArrayEqual(middle([1]), []));
-console.log(assertArrayEqual(middle([1, 2]), []));
-console.log(assertArrayEqual(middle([1, 2, 3]), [2]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
+describe('#middle', () => {
+  it('should return an empty array when input is an empty array', () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it ('should return an empty array when input array has a single value', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it ('should return an empty array when input array has two values', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it ('should return [2] when when given [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it ('should return [2, 3] when when given [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
