51304dfde3eff5396fd4044bb84c716bdd7f9467
Jinny Jang <jangjinny21@gmail.com>
2021-01-12 01:33:48 +0000

    added functions as key of an object being exported

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArrayEqual');
+const eqArrays = require('./eqArrays');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle
+  assertEqual,
+  assertArrayEqual,
+  eqArrays,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
